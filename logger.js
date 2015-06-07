d315eb23a98909a6f3d33771ca957f7171e5623b
OstlerDev <ostlerdev@me.com>
2015-06-07 15:33:41 -0600

    Start in Debug mode. Fixes #29
    You can now start PopcornTV in any mode you want, this is mainly used
    to put it in debug mode

--- logger.js
+++ logger.js
@@ -1,26 +1,39 @@
 var Winston = require('winston');
-var LoggingLevel = "Web";
+if (process.argv[2] != undefined){
+	var level = process.argv[2];
+} else {
+	var level = "Web";
+}
 
-var logger = new (Winston.Logger)({
-    levels: {Debug: 0, Web: 1, DNS: 1, Streamer: 1, notice: 2, warning: 3, error: 4},
-    transports: [
-        new Winston.transports.Console({
-            level: LoggingLevel,
-            colorize: true,
-            prettyPrint: true,
-            json: false
-        }),
-        new (Winston.transports.File)({ 
-        	filename: './PopcornTV.log', 
-        	level: LoggingLevel, 
-        	prettyPrint: true, 
-        	json: false,
-        	maxsize: 10 * 1024 * 1024,
-        	maxFiles: 3,
-        	tailable: true
-        })
-    ],
-    colors: {Debug: "red", Web: "cyan", DNS: "cyan", Streamer: "cyan", notice: "white", warning: "yellow", error: "red"}
+var logger = startLogger(level);
+
+process.argv.forEach(function (val, index, array) {
+  console.log(index + ': ' + val);
 });
+
+function startLogger(LoggingLevel){
+	var logger = new (Winston.Logger)({
+    	levels: {Debug: 0, Web: 1, DNS: 1, Streamer: 1, notice: 2, warning: 3, error: 4},
+	    transports: [
+    	    new Winston.transports.Console({
+        	    level: LoggingLevel,
+        	    colorize: true,
+        	    prettyPrint: true,
+        	    json: false
+        	}),
+        	new (Winston.transports.File)({ 
+        		filename: './PopcornTV.log', 
+        		level: LoggingLevel, 
+        		prettyPrint: true, 
+        		json: false,
+        		maxsize: 10 * 1024 * 1024,
+        		maxFiles: 3,
+        		tailable: true
+        	})
+    	],
+    	colors: {Debug: "red", Web: "cyan", DNS: "cyan", Streamer: "cyan", notice: "white", warning: "yellow", error: "red"}
+	});
+	return logger;
+}
 
 module.exports=logger;
