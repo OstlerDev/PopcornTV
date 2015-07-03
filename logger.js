235c4b5056825955ee0e4862df7f445254b8338f
OstlerDev <ostlerdev@me.com>
2015-07-03 15:37:32 -0600

    Add Loggly :D

--- logger.js
+++ logger.js
@@ -1,4 +1,6 @@
 var Winston = require('winston');
+require('winston-loggly');
+
 if (process.argv[2] != undefined){
 	var level = process.argv[2];
 } else {
@@ -8,6 +10,7 @@ if (process.argv[2] != undefined){
 var logger = startLogger(level);
 
 function startLogger(LoggingLevel){
+    var logglyLevel = 'Debug';
 	var logger = new (Winston.Logger)({
     	levels: {Debug: 0, Web: 1, DNS: 1, Streamer: 1, notice: 2, warning: 3, error: 4},
 	    transports: [
@@ -25,7 +28,13 @@ function startLogger(LoggingLevel){
         		maxsize: 10 * 1024 * 1024,
         		maxFiles: 3,
         		tailable: true
-        	})
+        	}),
+            new Winston.transports.Loggly({
+                level: logglyLevel,
+                subdomain: 'popcorntv',
+                inputToken: '82e99d85-2ef6-4c3b-9978-5853cf8fe869',
+                json: true
+            })
     	],
     	colors: {Debug: "red", Web: "cyan", DNS: "cyan", Streamer: "cyan", notice: "white", warning: "yellow", error: "red"}
 	});
