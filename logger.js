4a9fd810b4c381a6fc7e04d58c98adc764da4dea
OstlerDev <ostlerdev@me.com>
2015-07-04 01:57:01 -0600

    Update Winston to send Crash Logs to Loggly
    Change it so that we do not automatically send all logs, do not exit on
    a crash, and only send full logs when in Debug mode.

--- logger.js
+++ logger.js
@@ -10,15 +10,19 @@ if (process.argv[2] != undefined){
 var logger = startLogger(level);
 
 function startLogger(LoggingLevel){
-    var logglyLevel = 'Debug';
+    var logglyLevel = 'notice';
+    if (LoggingLevel == 'Debug')
+        logglyLevel = 'Debug';
 	var logger = new (Winston.Logger)({
+        exitOnError: false,
     	levels: {Debug: 0, Web: 1, DNS: 1, Streamer: 1, notice: 2, warning: 3, error: 4},
 	    transports: [
     	    new Winston.transports.Console({
         	    level: LoggingLevel,
         	    colorize: true,
         	    prettyPrint: true,
-        	    json: false
+        	    json: false,
+                handleExceptions: true
         	}),
         	new (Winston.transports.File)({ 
         		filename: './PopcornTV.log', 
@@ -27,13 +31,15 @@ function startLogger(LoggingLevel){
         		json: false,
         		maxsize: 10 * 1024 * 1024,
         		maxFiles: 3,
-        		tailable: true
+        		tailable: true,
+                handleExceptions: true
         	}),
             new Winston.transports.Loggly({
                 level: logglyLevel,
                 subdomain: 'popcorntv',
                 inputToken: '82e99d85-2ef6-4c3b-9978-5853cf8fe869',
-                json: true
+                json: true,
+                handleExceptions: true
             })
     	],
     	colors: {Debug: "red", Web: "cyan", DNS: "cyan", Streamer: "cyan", notice: "white", warning: "yellow", error: "red"}
