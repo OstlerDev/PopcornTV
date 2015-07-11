73db8d52f1ceed428830a909ec5a523db9447d58
John Freeman <john@brainkernel.com>
2015-07-11 09:52:59 -0500

    Functionfied the server startup code.

--- atv.js
+++ atv.js
@@ -3,6 +3,21 @@ var dns = require('./dns');
 var ip = require("ip");
 var logger = require('./logger');
 
+startServers = function(data) {
+	try {
+        config = JSON.parse(data);
+        const LOCAL_IP = config.ip;
+        logger.notice("Starting PopcornTV");
+        dns.startDnsProxy(LOCAL_IP);
+        webservers.startWebServer(LOCAL_IP);
+        webservers.startSSLWebServer(LOCAL_IP);
+    } catch (err) {
+        logger.error('There is an error starting Popcorn TV, please post this on the Github page')
+        console.error(err);
+        process.exit();
+    }
+}
+
 var fs = require('fs');
 if (!fs.existsSync('assets/certificates/trailers.cer')){
     var pem = require('pem');
@@ -17,36 +32,24 @@ if (!fs.existsSync('assets/certificates/trailers.cer')){
                 logger.warning('If you are on Windows, please follow this workaround: http://bit.ly/1H7qfJZ')
         }
     });
+} else if (fs.existsSync('config.json')) {
+    var data = fs.readFileSync('./config.json'), config;
+    startServers(data);
 } else {
-	var data = null;
-	var config = null;
-	if (fs.existsSync('config.json')) {
-		var data = fs.readFileSync('./config.json'), config;
-	} else {
-		var myOptions = {
-		        ip: ip.address(),
-		        default_dns: '8.8.8.8'
-		    };
-		    var data = JSON.stringify(myOptions, null, 4);
-		    fs.writeFile('./config.json', data, function(err) {
-		        if (err) {
-		            logger.error('There has been an error generating the Config, please report this error on the github page!');
-		            logger.error(err.message);
-		            return;
-		        }
-		    });
-		    var data = fs.readFileSync('./config.json'), config;
-	}
-	try {
-		config = JSON.parse(data);
-		const LOCAL_IP = config.ip;
-		logger.notice("Starting PopcornTV");
-		dns.startDnsProxy(LOCAL_IP);
-		webservers.startWebServer(LOCAL_IP);
-		webservers.startSSLWebServer(LOCAL_IP);
-    } catch (err) {
-        logger.error('There is an error starting Popcorn TV, please post this on the Github page')
-        logger.error(err);
-        process.exit();
-    }
+    var myOptions = {
+        ip: ip.address(),
+        default_dns: '8.8.8.8'
+    };
+
+    var data = JSON.stringify(myOptions, null, 4);
+
+    fs.writeFile('./config.json', data, function(err) {
+        if (err) {
+            logger.error('There has been an error generating the Config, please report this error on the github page!');
+            logger.error(err.message);
+            return;
+        }
+        var data = fs.readFileSync('./config.json'), config;
+        startServers(data);
+    });
 }
