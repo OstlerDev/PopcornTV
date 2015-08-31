0fd114df9e76f5ad06ddf5f25cd1fe6dea8e0e5c
OstlerDev <ostlerdev@me.com>
2015-08-31 17:34:30 -0600

    Push up Basic Trakt

---
+++ trakt.js
@@ -0,0 +1,32 @@
+function scrobbleStart(imdb, type){
+    var url = 'https://api-v2launch.trakt.tv/scrobble/start';
+    makeRequest(url, function(body){
+        console.log(body);
+    })
+}
+
+function makeRequest(url, callback){
+    var request = require('request');
+    
+    logger.Debug(url);
+    request.post({
+        url: url,
+        json: true,
+        headers: {
+            'Content-Type': 'application/json',
+            'authorization': 'Bearer 07e5793def102a012f19af929360d75dd4af5a14b97ce8e61698adbbd14ef2c6',
+            'trakt-api-version': '2',
+            'trakt-api-key': '8e798f3c3ed286081991f459f3d8fcb4e40969a31ce29f1f08e0ac4dbaf49258'
+        },
+        body: {"movie": {"ids": {"imdb": 'tt0468569'}}}
+    }, function (error, response, body) {
+       if (!error && response.statusCode === 200) {
+           callback(body);
+        } else {
+            console.warning("Error connecting to trakt.tv and grabbing json: " + url);
+            return;
+        }
+    })
+}
+
+scrobbleStart('tt0468569', 'movie');
